e5206e46b3102abf888341970c238fe7d98cdf2e
Louis BOSSY <louis.bossy@etu.univ-tours.fr>
2025-12-10 15:42:38 +0100

    added the inteface for MapController

---
+++ frontend/src/app/services/map_controllers/MapController.ts
@@ -0,0 +1,32 @@
+import * as L from 'leaflet';
+
+class MapController {
+    /**
+     * A template used by map "controllers", aka. classes that provide a specific
+     * feature to the map by updating layers and/or handling given user interactions
+     */
+
+    map: L.Map;
+    layers: L.Layer[] = [];
+
+    constructor(map: L.Map) {
+        /**
+         * @param map the leaflet map object to bind to the controller
+         */
+        this.map = map;
+    }
+    refreshData(): void {
+        /**
+         * force the controller to update its data according to the current state of the map
+         */
+    }
+
+    //[ Interactions methods ]//
+    onSimpleClick(): void {}
+    onLongClick(): void {}
+    onDoubleClick(): void {}
+    onSimplePanMove(): void {}
+    onDoublePanMove(): void {}
+    onPinch(): void {}
+    onSwipe(): void {}
+}
